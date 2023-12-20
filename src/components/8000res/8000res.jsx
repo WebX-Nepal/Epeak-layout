6f9417a26dc1dd6d1c6e216153ae576bd66ab18a
AshishLawaju <ashish.lawaju74@gmail.com>
2023-12-20 13:40:11 +0545

    8 mountains added

--- src/components/8000res/8000res.jsx
+++ src/components/8000res/8000res.jsx
@@ -4,16 +4,50 @@
 
 import { useState } from "react";
 import dhaulagiri from "./dhaulagiri.svg";
+import everest from "./everest.svg";
+import k2 from "./k2.svg";
+import kangchenjunga from "./kangchenjunga.svg";
+import lhotse from "./lhotse.svg";
+import makalu from "./makalu.svg";
+import Chooyu from "./Chooyu.svg";
 import "./peak.css";
 import Mcard from "./Mcard";
 
 export const Eight = () => {
   const [showDhaulagiri, setShowDhulagiri] = useState(false);
+  const [showEverest, setShowEverest] = useState(false);
+  const [showK2, setShowK2] = useState(false);
+  const [showKangchenjunga, setShowKangchenjunga] = useState(false);
+  const [showLhotse, setShowLhotse] = useState(false);
+  const [showMakalu, setShowMalaku] = useState(false);
+  const [showChooyu, setShowChooyu] = useState(false);
+  const [showManaslu, setShowManaslu] = useState(false);
+
   return (
-    <section className={`w-full h-[100vh]  bg-[#f7f7f7] flex items-center relative`}>
-      <div className="px-40 flex flex-wrap">
+    <section className={`w-full    flex items-center relative`}>
+      <div className="px-40 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 border w-full">
         <div
-          className="w-[220px] cursor-pointer "
+          className="w-[220px] cursor-pointer relative"
+          onMouseEnter={() => {
+            setShowEverest(true);
+          }}
+          onMouseLeave={() => {
+            setShowEverest(false);
+          }}
+        >
+          <img src={everest} alt="" />
+          {showEverest && (
+            <Mcard
+              name={"Mount Everest"}
+              subTitle={"submit your dream with us"}
+              height={"8848.86"}
+            />
+          )}
+        </div>
+        {/* ss */}
+
+        <div
+          className="w-[220px] cursor-pointer relative"
           onMouseEnter={() => {
             setShowDhulagiri(true);
           }}
@@ -21,52 +55,120 @@ export const Eight = () => {
             setShowDhulagiri(false);
           }}
         >
+          {showDhaulagiri && (
+            <Mcard
+              name={"Dhaulagiri"}
+              subTitle={"submit your dream with us"}
+              height={" 8167"}
+            />
+          )}
           <img src={dhaulagiri} alt="" />
-          {showDhaulagiri && <Mcard />}
         </div>
-        {/* ss */}
-
         <div
-          className="w-[220px] cursor-pointer "
+          className="w-[220px] cursor-pointer relative"
+          onMouseEnter={() => {
+            setShowKangchenjunga(true);
+          }}
+          onMouseLeave={() => {
+            setShowKangchenjunga(false);
+          }}
+        >
+          {showKangchenjunga && (
+            <Mcard
+              name={"Kangchenjunga"}
+              subTitle={"submit your dream with us"}
+              height={" 8007"}
+            />
+          )}
+          <img src={kangchenjunga} alt="" />
+        </div>
+        <div
+          className="w-[220px] cursor-pointer relative"
+          onMouseEnter={() => {
+            setShowK2(true);
+          }}
+          onMouseLeave={() => {
+            setShowK2(false);
+          }}
+        >
+          {showK2 && (
+            <Mcard
+              name={"Annapurna"}
+              subTitle={"submit your dream with us"}
+              height={"  8091"}
+            />
+          )}
           
-        >
-          <img src={dhaulagiri} alt="" />
-          {/* {showDhaulagiri && <Mcard />} */}
+          <img src={k2} alt="" />
         </div>
         <div
-          className="w-[220px] cursor-pointer "
-          
+          className="w-[220px] cursor-pointer relative"
+          onMouseEnter={() => {
+            setShowLhotse(true);
+          }}
+          onMouseLeave={() => {
+            setShowLhotse(false);
+          }}
         >
-          <img src={dhaulagiri} alt="" />
-          {/* {showDhaulagiri && <Mcard />} */}
+          {showLhotse && (
+            <Mcard
+              name={"Lhotse"}
+              subTitle={"submit your dream with us"}
+              height={"  8077"}
+            />
+          )}
+          <img src={lhotse} alt="" />
+        </div>
+        <div className="w-[220px] cursor-pointer relative "
+        onMouseEnter={() => {
+          setShowManaslu(true);
+        }}
+        onMouseLeave={() => {
+          setShowManaslu(false);
+        }}>
+          <img src={k2} alt="" />
+          {showManaslu && (
+            <Mcard
+              name={"Manaslu"}
+              subTitle={"submit your dream with us"}
+              height={"  8055"}
+            />
+          )}
         </div>
         <div
-          className="w-[220px] cursor-pointer "
-        
+          className="w-[220px] cursor-pointer relative"
+          onMouseEnter={() => {
+            setShowMalaku(true);
+          }}
+          onMouseLeave={() => {
+            setShowMalaku(false);
+          }}
         >
-          <img src={dhaulagiri} alt="" />
-          {/* {showDhaulagiri && <Mcard />} */}
+            {showMakalu && (
+            <Mcard
+              name={"Makalu"}
+              subTitle={"submit your dream with us"}
+              height={"  8391"}
+            />
+          )}
+          <img src={makalu} alt="" />
         </div>
         <div
-          className="w-[220px] cursor-pointer "
-       
-        >
-          <img src={dhaulagiri} alt="" />
-          {/* {showDhaulagiri && <Mcard />} */}
-        </div>
-        <div
-          className="w-[220px] cursor-pointer "
-       
-        >
-          <img src={dhaulagiri} alt="" />
-          {/* {showDhaulagiri && <Mcard />} */}
-        </div>
-        <div
-          className="w-[220px] cursor-pointer "
-         
-        >
-          <img src={dhaulagiri} alt="" />
-          {/* {showDhaulagiri && <Mcard />} */}
+          className="w-[220px] cursor-pointer relative"
+          onMouseEnter={() => {
+            setShowChooyu(true);
+          }}
+          onMouseLeave={() => {
+            setShowChooyu(false);
+          }}
+        >  {showChooyu && (
+          <Mcard
+            name={"Annapurna"}
+            subTitle={"submit your dream with us"}
+            height={"  8291"}
+          />
+        )}
+          <img src={Chooyu} alt="" />
         </div>
       </div>
     </section>
--- src/components/8000res/Mcard.jsx
+++ src/components/8000res/Mcard.jsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom";
+export default function Mcard({imageLink,name,height,subTitle}) {
 
-export default function Mcard() {
   return (
-    <div className="w-[330px] h-[180px] bg-[#4a97da] rounded-3xl absolute  top-0 flex  p-2">
-        <div className="w-[50%]  bg-[url('https://images.unsplash.com/photo-1509883488717-779cd2d85976?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover p bg-center h-full   rounded-3xl"> 
-            <img src="" alt="" />
-        </div>
-        <div className="text-white text-center py-3 pl-2 p"> 
-            <h3 className="text-lg"> Mount Everest</h3>
-            <p className="text-sm ">(8848.86 meters) </p>
-            <p className="text-xs ">submit your dream with us </p>
-            <button className="bg-white text-[#4a97da] rounded-3xl px-3 py-1 mt-4">Explore more</button>
-        </div>
-        
+    <div className="w-[330px] cursor-default h-[180px] bg-[#4a97da] rounded-3xl absolute  -top-[140px] flex  p-2">
+      <div className="w-[50%]  bg-[url('https://images.unsplash.com/photo-1509883488717-779cd2d85976?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover p bg-center h-full   rounded-3xl">
+        <img src="" alt="" />
+      </div>
+      <div className="text-white text-center py-3 pl-2 p">
+        <h3 className="text-lg">{name} </h3>
+        <p className="text-sm ">({height} meters) </p>
+        <p className="text-xs ">{subTitle} </p>
+
+        <Link to="/everest">
+        <button className="bg-white text-[#4a97da] rounded-3xl px-3 py-1 mt-4">
+          Explore more
+        </button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
