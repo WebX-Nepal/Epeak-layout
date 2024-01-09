f1e2ea70ec76b087d7ccb78f28c8cee77f12b012
AshishLawaju <ashish.lawaju74@gmail.com>
2024-01-09 17:45:07 +0545

    ss

--- src/pages/Itinerary/components/PV.jsx
+++ src/pages/Itinerary/components/PV.jsx
@@ -1,4 +1,15 @@
 import { Icon } from "@iconify/react";
+import G1 from "./assets/G1.png";
+import G2 from "./assets/G2.png";
+import G3 from "./assets/G3.png";
+import G4 from "./assets/G4.png";
+import LightGallery from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import "./gallary.css"
 export default function PV() {
   return (
     <section className="py-32">
@@ -13,7 +24,24 @@ export default function PV() {
       </div>
 
 
-        
+
+      <div className="mt-12  gall">
+        <LightGallery  speed={500} plugins={[lgThumbnail]} >
+s
+          <a href={G1}>
+            <img src={G1} alt="" />
+          </a>
+          <a href={G2}>
+            <img src={G2} alt="" />
+          </a>
+          <a href={G3}>
+            <img src={G3} alt="" />
+          </a>
+          <a href={G4}>
+            <img src={G4} alt="" />
+          </a>
+        </LightGallery>
+      </div>
     </section>
   );
 }
